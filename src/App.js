833518f7e0b31b6e9e382bc5515b7015bef820c4
nhattpm98 <66395674+nhattpm6598@users.noreply.github.com>
2024-06-14 14:30:56 +0700

    #update code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
 
 function App() {
+  const name = "nhattpm";
+
+  const age = 18;
+
+  const isMale = true;
+
+  const student = { name: "Hello Word" };
+
+  const colorList = ["red", "greeen", "blue"];
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>! Hello Word !!</p>
         <p>
-          Nhattpm ! Hello Word !!
+          {name} - {age}
         </p>
+        {isMale ? <p>'Male'</p> : <p>'Female'</p>}
+
+        {isMale && <p>'Male'</p>}
+        {!isMale && (
+          <React.Fragment>
+            <p>'Female'</p>
+            <p>'Female'</p>
+          </React.Fragment>
+        )}
+
+        <p>{student.name}</p>
+
+        <ul>
+          {colorList.map((color) => (
+            <li style={{color}}>{color}</li>
+          ))}
+        </ul>
       </header>
     </div>
   );
